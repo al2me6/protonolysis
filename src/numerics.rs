be9163c583d0004a96fe896762bed10a5f4bfa64
Alvin Meng <alvin.q.meng@gmail.com>
2023-05-08 13:34:46 -0400

    feat: improve GaussianSum::{extent,max}

--- src/numerics.rs
+++ src/numerics.rs
@@ -45,28 +45,32 @@ impl FromIterator<Gaussian> for GaussianSum {
 }
 
 impl GaussianSum {
+    /// Iterate over the individual Gaussians of the sum.
+    pub fn components(&self) -> impl Iterator<Item = &Gaussian> {
+        self.0.iter()
+    }
+
     #[must_use]
     pub fn evaluate(&self, x: f64) -> f64 {
-        self.0.iter().map(|g| g.evaluate(x)).sum()
+        self.components().map(|g| g.evaluate(x)).sum()
     }
 
     #[must_use]
+    /// The overall extent of the sum, or the union of the extents of the individual components
+    /// (where each extent comprises the interval `σ` standard deviations out from the mean).
     pub fn extent(&self, σ: f64) -> RangeInclusive<f64> {
-        // FIXME: handle overlapping components.
-        if self.0.is_empty() {
-            0.0..=0.0
-        } else {
-            let (first, last) = (self.0.first().unwrap(), self.0.last().unwrap());
-            (first.μ - first.σ * σ)..=(last.μ + last.σ * σ)
-        }
+        self.components()
+            .map(|g| (g.μ - g.σ * σ, g.μ + g.σ * σ))
+            .reduce(|(l1, r1), (l2, r2)| (l1.min(l2), r1.max(r2)))
+            .map_or(0.0..=0.0, |(l, r)| l..=r)
     }
 
     #[must_use]
+    /// Give an _estimate_ of the max value of the sum, by evaluating the sum at the maxima
+    /// (i.e., means) of the components.
     pub fn max(&self) -> f64 {
-        // FIXME: handle overlapping components.
-        self.0
-            .iter()
-            .map(|g| g.evaluate(g.μ))
+        self.components()
+            .map(|g| self.evaluate(g.μ))
             .reduce(f64::max)
             .unwrap_or(0.)
     }
