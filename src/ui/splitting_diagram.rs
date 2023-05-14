e03e1a01f4a558022468cc57f7cd36314ba95472
Alvin Meng <alvin.q.meng@gmail.com>
2023-05-14 15:49:46 -0400

    design: switch to thinner dashed line

--- src/ui/splitting_diagram.rs
+++ src/ui/splitting_diagram.rs
@@ -7,16 +7,6 @@ const STAGE_ORIGIN: f64 = 0.;
 const STAGE_HEIGHT: f64 = 50.;
 const MAX_PEAKLET_HEIGHT: f64 = 35.;
 
-const PEAKLET_THICKNESS: f32 = 3.;
-const CONNECTING_LINE_THICKNESS: f32 = 1.;
-
-const PEAKLET_COLOR: Color32 = Color32::LIGHT_BLUE;
-const DISABLED_PEAKLET_COLOR: Color32 = Color32::DARK_GRAY;
-const CONNECTOR_COLOR: Color32 = Color32::GRAY;
-const DISABLED_CONNECTOR_COLOR: Color32 = Color32::DARK_GRAY;
-
-const CONNECTOR_STYLE: LineStyle = LineStyle::Dotted { spacing: 5. };
-
 #[allow(clippy::cast_precision_loss)]
 fn base_height_of(stage: usize) -> f64 {
     STAGE_ORIGIN - stage as f64 * STAGE_HEIGHT
@@ -33,18 +23,17 @@ pub(super) fn draw_peaklet_marker(
     max_integration: f64,
     enabled: bool,
 ) {
-    let base = base_height_of(stage);
     plot_ui.line(
         Line::new(vec![
-            [peaklet.δ, base],
+            [peaklet.δ, base_height_of(stage)],
             [peaklet.δ, tip_height_of(peaklet, stage, max_integration)],
         ])
         .color(if enabled {
-            PEAKLET_COLOR
+            Color32::LIGHT_BLUE
         } else {
-            DISABLED_PEAKLET_COLOR
+            Color32::DARK_GRAY
         })
-        .width(PEAKLET_THICKNESS),
+        .width(3.),
     );
 }
 
@@ -56,7 +45,6 @@ pub(super) fn draw_group_children_and_connectors(
     enabled: bool,
 ) {
     let parent_base = [group.parent.δ, base_height_of(stage - 1)];
-    // let max_integration = group.
     for child in group.children {
         draw_peaklet_marker(plot_ui, child, stage, max_integration, enabled);
         let child_tip = [child.δ, tip_height_of(child, stage, max_integration)];
@@ -64,12 +52,12 @@ pub(super) fn draw_group_children_and_connectors(
         plot_ui.line(
             Line::new(vec![child_tip, corner, parent_base])
                 .color(if enabled {
-                    CONNECTOR_COLOR
+                    Color32::GRAY
                 } else {
-                    DISABLED_CONNECTOR_COLOR
+                    Color32::DARK_GRAY
                 })
-                .style(CONNECTOR_STYLE)
-                .width(CONNECTING_LINE_THICKNESS),
+                .style(LineStyle::dashed_dense())
+                .width(1.),
         );
     }
 }
