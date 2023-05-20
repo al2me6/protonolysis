5115da7bedaeae5db92835d5c5ad7ab2d2960c88
Alvin Meng <alvin.q.meng@gmail.com>
2023-05-20 17:39:40 -0400

    feat: implement multi-touch plot interaction

--- src/ui/utils.rs
+++ src/ui/utils.rs
@@ -1,42 +1,50 @@
 use eframe::egui::plot::{PlotBounds, PlotUi};
-use eframe::egui::{self, CursorIcon, Margin, TopBottomPanel, Ui};
+use eframe::egui::{self, CursorIcon, InputState, Margin, TopBottomPanel, Ui};
 use eframe::epaint::Vec2;
 
 /// Apply custom zoom and pan interactions for peak plots.
 pub(super) fn peak_viewer_interactions(plot_ui: &mut PlotUi, x_axis: &mut (f64, f64)) {
-    // FIXME: touch support.
-
     if !plot_ui.plot_hovered() {
         return;
     }
+
+    let multitouch = plot_ui.ctx().input(InputState::multi_touch);
+
+    // Custom zoom:
+    let bounds = plot_ui.plot_bounds();
+    let (mut bounds_min, mut bounds_max) = (bounds.min(), bounds.max());
+    // scroll wheel y zoom:
+    let raw_scroll_y = plot_ui.ctx().input(|i| f64::from(i.scroll_delta.y));
+    if raw_scroll_y != 0. {
+        let scroll_y = (raw_scroll_y / 200.).exp();
+        bounds_min[1] /= scroll_y;
+        bounds_max[1] /= scroll_y;
+    }
+    // ctrl-scroll x-zoom or pinch-to-zoom (x and y):
+    // This seems to eat the raw scroll delta in the former case.
+    let zoom_delta = plot_ui.ctx().input(InputState::zoom_delta_2d);
+    let zoom_x = f64::from(zoom_delta.x);
+    bounds_min[0] /= zoom_x;
+    bounds_max[0] /= zoom_x;
+    if multitouch.is_some() {
+        let pinch_y = f64::from(zoom_delta.y);
+        bounds_min[1] /= pinch_y;
+        bounds_max[1] /= pinch_y;
+    }
+    *x_axis = (bounds_min[0], bounds_max[0]);
+    plot_ui.set_plot_bounds(PlotBounds::from_min_max(bounds_min, bounds_max));
 
     // Custom pan:
     let drag = plot_ui
         .ctx()
         .input(|i| i.pointer.primary_down().then(|| i.pointer.delta()));
     if let Some(drag) = drag {
-        plot_ui.ctx().set_cursor_icon(CursorIcon::ResizeHorizontal);
-        plot_ui.translate_bounds(Vec2 { x: -drag.x, y: 0. });
+        // Don't allow drag-to-pan while in pinch-to-zoom.
+        if multitouch.is_none() {
+            plot_ui.ctx().set_cursor_icon(CursorIcon::ResizeHorizontal);
+            plot_ui.translate_bounds(Vec2 { x: -drag.x, y: 0. });
+        }
     }
-
-    // Custom zoom:
-    let bounds = plot_ui.plot_bounds();
-    let (mut bounds_min, mut bounds_max) = (bounds.min(), bounds.max());
-    // y: zoom:
-    let scroll_y = plot_ui.ctx().input(|i| f64::from(i.scroll_delta.y));
-    if scroll_y != 0. {
-        let zoom_factor = (scroll_y / 200.).exp();
-        bounds_min[1] /= zoom_factor;
-        bounds_max[1] /= zoom_factor;
-    }
-    // x zoom (ctrl+scroll):
-    // This seems to eat the raw scroll delta.
-    let ctrl_scroll_factor = plot_ui.ctx().input(|i| f64::from(i.zoom_delta()));
-    bounds_min[0] /= ctrl_scroll_factor;
-    bounds_max[0] /= ctrl_scroll_factor;
-    // Apply:
-    *x_axis = (bounds_min[0], bounds_max[0]);
-    plot_ui.set_plot_bounds(PlotBounds::from_min_max(bounds_min, bounds_max));
 }
 
 pub(super) fn inner_bottom_panel(
